8bb5ad13fc7bf0475ea717cfb317563e79f30603
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-06-27 01:42:55 +0100

    Added docs for CommandConfiguration class

--- src/main/java/jara/CommandConfiguration.java
+++ src/main/java/jara/CommandConfiguration.java
@@ -18,40 +18,69 @@ public class CommandConfiguration
 		aliases = aliasesArg;
 		commandClass = commandClassArg;
 	}
-	
+	/**
+	 * 
+	 * Simple get method for the command's corresponding class.
+	 * Do not use this method for instanciating a command as it
+	 * does not perform an enabled check. 
+	 * 
+	 * Instead, use execute()
+	 */
 	public Class<? extends Command> getCommandClass()
 	{
 		return commandClass;
 	}
-	
-	public void execute(GuildMessageReceivedEvent msgEvent, String...parameters)
+	/**
+	 * 
+	 * @param msgEvent
+	 * @param parameters
+	 * 
+	 * Creates a new instance of the command and runs it.
+	 * 
+	 * Returns:
+	 * true - Command instanciated successfully
+	 * false - Error occured. Check the console for details. Command is not run.
+	 */
+	public boolean execute(GuildMessageReceivedEvent msgEvent, String...parameters)
 	{
 		if (isEnabled())
 		{
 			try
 			{
 				getCommandClass().newInstance().run(msgEvent, parameters);
-			} catch (InstantiationException | IllegalAccessException e)
+				return true;
+			} 
+			catch (InstantiationException | IllegalAccessException e)
 			{
 				msgEvent.getChannel().sendMessage("Sorry, I was unable to run the command.").queue();
 				Logger logger = LoggerFactory.getLogger(CommandConfiguration.class);
 				logger.error("A command request was sent but could not be fulfilled: "+parameters.toString()); //TODO: Provide more details (Guild, user, channel, etc.)
 				e.printStackTrace();
+				return false;
 			}
 		}
 		else
 		{
 			Logger logger = LoggerFactory.getLogger(CommandConfiguration.class);
 			logger.info("The command called with "+parameters[0]+" is disabled. Please enable it in the config.");
+			return false;
 		}
 
 	}
-	
+	/**
+	 * 
+	 * Simple get for all the different text strings
+	 * that will call the command.
+	 */
 	public String[] getAliases()
 	{
 		return aliases;
 	}
-	
+	/**
+	 * 
+	 * Reports back if the command is enabled in the
+	 * config. If this returns false, execute will also fail.
+	 */
 	public boolean isEnabled()
 	{
 		return enabledState;
