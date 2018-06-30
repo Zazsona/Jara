d89128d286aac45e2c7777dc30784991d5cb107d
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-06-30 20:00:49 +0100

    Added Joke command

--- src/main/java/jara/Boot.java
+++ src/main/java/jara/Boot.java
@@ -1,5 +1,6 @@
 package jara;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class Boot 
@@ -7,6 +8,7 @@ public class Boot
 	public static void main(String[] args)
 	{
 		//The idea here is that new elements unrelated to Discord (External dash, API starts, config setting, etc. can all be called here)
+		firstTimeSetup();
 		Core.initialiseDiscordConnection(args[0]); //Pass the client token.
 		HashMap<String, Boolean> temp = new HashMap<String, Boolean>();
 		temp.put("Ping", true);						//TODO: Read this HashMap from a config file
@@ -14,6 +16,11 @@ public class Boot
 		temp.put("About", true);
 		temp.put("CoinFlip", true);
 		temp.put("EightBall", true);
+		temp.put("Joke", true);
 		Core.enableCommands(temp); 
+	}
+	private static void firstTimeSetup()
+	{
+		File directory;
 	}
 }
