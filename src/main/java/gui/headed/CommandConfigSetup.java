bcef3c9c61d147f1b088e3104a01ef046c8c7b4a
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-09-17 17:48:30 +0100

    Populate setup's command list, add functionality to checkboxes.

--- src/main/java/gui/headed/CommandConfigSetup.java
+++ src/main/java/gui/headed/CommandConfigSetup.java
@@ -1,15 +1,20 @@
 package gui.headed;
 
+import jara.CommandAttributes;
+import jara.CommandRegister;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
+import javafx.scene.control.CheckBox;
 
 import java.io.IOException;
 
@@ -32,6 +37,24 @@ public class CommandConfigSetup extends Application
     private Text navBar_review_text;
     @FXML
     private VBox adminList;
+    @FXML
+    private VBox gamesList;
+    @FXML
+    private VBox toysList;
+    @FXML
+    private VBox utilityList;
+    @FXML
+    private VBox audioList;
+    @FXML
+    private CheckBox adminCategoryCheckBox;
+    @FXML
+    private CheckBox audioCategoryCheckBox;
+    @FXML
+    private CheckBox gamesCategoryCheckBox;
+    @FXML
+    private CheckBox toysCategoryCheckBox;
+    @FXML
+    private CheckBox utilityCategoryCheckBox;
 
     public void initialize()
     {
@@ -45,9 +68,52 @@ public class CommandConfigSetup extends Application
 
         navBar_discord_text.setOnMouseClicked((event) -> HeadedUtil.manageTitleSelection(navBar_discord_text));
 
-        navBar_welcome_text.setOnMouseClicked((event -> HeadedUtil.manageTitleSelection(navBar_welcome_text)));
+        navBar_welcome_text.setOnMouseClicked((event) -> HeadedUtil.manageTitleSelection(navBar_welcome_text));
 
-        navBar_review_text.setOnMouseClicked((event -> HeadedUtil.manageTitleSelection(navBar_review_text)));
+        navBar_review_text.setOnMouseClicked((event) -> HeadedUtil.manageTitleSelection(navBar_review_text));
+
+        adminCategoryCheckBox.setOnMouseClicked((event) -> toggleCategory(adminCategoryCheckBox, CommandRegister.ADMIN));
+        audioCategoryCheckBox.setOnMouseClicked((event) -> toggleCategory(audioCategoryCheckBox, CommandRegister.AUDIO));
+        gamesCategoryCheckBox.setOnMouseClicked((event) -> toggleCategory(gamesCategoryCheckBox, CommandRegister.GAMES));
+        toysCategoryCheckBox.setOnMouseClicked((event) -> toggleCategory(toysCategoryCheckBox, CommandRegister.TOYS));
+        utilityCategoryCheckBox.setOnMouseClicked((event) -> toggleCategory(utilityCategoryCheckBox, CommandRegister.UTILITY));
+
+        for (CommandAttributes ca : CommandRegister.getRegister())
+        {
+            Pane topSpacePane = new Pane();
+            topSpacePane.setMinHeight(5);
+            topSpacePane.setMaxHeight(10);
+            topSpacePane.setPrefHeight(10);
+            Pane bottomSpacePane = new Pane();
+            bottomSpacePane.setMinHeight(topSpacePane.getMinHeight());
+            bottomSpacePane.setMaxHeight(topSpacePane.getMaxHeight());
+            bottomSpacePane.setPrefHeight(topSpacePane.getPrefHeight());
+
+            switch (ca.getCategoryID())
+            {
+                case CommandRegister.GAMES:
+                    gamesList.getChildren().addAll(topSpacePane, generateCommandListElement(ca), bottomSpacePane);
+                    break;
+                case CommandRegister.UTILITY:
+                    utilityList.getChildren().addAll(topSpacePane, generateCommandListElement(ca), bottomSpacePane);
+                    break;
+                case CommandRegister.TOYS:
+                    toysList.getChildren().addAll(topSpacePane, generateCommandListElement(ca), bottomSpacePane);
+                    break;
+                case CommandRegister.AUDIO:
+                    audioList.getChildren().addAll(topSpacePane, generateCommandListElement(ca), bottomSpacePane);
+                    break;
+                case CommandRegister.ADMIN:
+                    adminList.getChildren().addAll(topSpacePane, generateCommandListElement(ca), bottomSpacePane);
+                    break;
+            }
+            if (ca.getCategoryID() == CommandRegister.ADMIN)
+            {
+
+
+
+            }
+        }
 
     }
     @Override
@@ -65,4 +131,79 @@ public class CommandConfigSetup extends Application
             e.printStackTrace();
         }
     }
+    private BorderPane generateCommandListElement(CommandAttributes commandAttributes)
+    {
+        BorderPane bp = new BorderPane();
+        Pane topPane = new Pane();
+        topPane.setStyle("-fx-background-color: #36393F;");
+        topPane.setMinHeight(2);
+        topPane.setMaxHeight(2);
+        topPane.setPrefWidth(270);
+        bp.setTop(topPane);
+        Pane bottomPane = new Pane();
+        bottomPane.setStyle(topPane.getStyle());
+        bottomPane.setMinHeight(topPane.getMinHeight());
+        bottomPane.setMaxHeight(topPane.getMaxHeight());
+        bottomPane.setPrefWidth(topPane.getPrefWidth());
+        bp.setBottom(bottomPane);
+        CheckBox checkBox = new CheckBox();
+        checkBox.setSelected(true);
+        if (!commandAttributes.isDisableable())
+        {
+            checkBox.setDisable(true);
+        }
+        bp.setRight(checkBox);
+        bp.setAlignment(checkBox, Pos.CENTER);
+        VBox vbox = new VBox();
+        Text nameText = new Text(commandAttributes.getCommandKey());
+        Text descText = new Text("Placeholder Desc");
+        nameText.setFont(new Font("System", 12));
+        nameText.setStyle("-fx-font-weight: bold;");
+        nameText.setFill(Paint.valueOf("#FFFFFF"));
+        descText.setFont(new Font("System", 12));
+        descText.setFill(Paint.valueOf("#FFFFFF"));
+        vbox.getChildren().addAll(nameText, descText);
+        bp.setCenter(vbox);
+        return bp;
+
+    }
+    private void toggleCategory(CheckBox categoryCheckBox, int categoryID)
+    {
+        VBox list;
+        switch (categoryID)
+        {
+            case CommandRegister.GAMES:
+                list = gamesList;
+                break;
+            case CommandRegister.UTILITY:
+                list = utilityList;
+                break;
+            case CommandRegister.TOYS:
+                list = toysList;
+                break;
+            case CommandRegister.AUDIO:
+                list = audioList;
+                break;
+            case CommandRegister.ADMIN:
+                list = adminList;
+                break;
+            default:
+                list = new VBox();
+        }
+        for (Node node : list.getChildren())
+        {
+            if (node instanceof BorderPane)
+            {
+                CheckBox checkBox = ((CheckBox) ((BorderPane) node).getRight());
+                if (!checkBox.isDisabled())
+                {
+                    checkBox.setSelected(categoryCheckBox.isSelected());
+                }
+            }
+        }
+    }
+    //TODO: Save state (CommandConfiguration instance?)
+    //TODO: Restore state
+
+
 }
