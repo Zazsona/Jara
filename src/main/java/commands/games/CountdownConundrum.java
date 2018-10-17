876f76326973359decdc5d157274a6bd3b7414ce
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-10-17 01:22:19 +0100

    Refactor how CountdownConundrum's conundrums work.
    - Load conundrums from external file rather than storing them in memory
    - Randomise the characters on the fly, making it less about memorising the jumbles
    - Add many new conundrums
    
    While one option would be to take a 9 letter word from wordList, the problem with this would be some of those words are... far from regular vocabulary.

--- src/main/java/commands/games/CountdownConundrum.java
+++ src/main/java/commands/games/CountdownConundrum.java
@@ -7,11 +7,22 @@ import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
 public class CountdownConundrum extends Command
 {
+    private static Logger logger = LoggerFactory.getLogger(CountdownConundrum.class);
     /**
      * The time it took to guess the conundrum
      */
@@ -30,187 +41,144 @@ public class CountdownConundrum extends Command
     public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
     {
         TextChannel channel = super.createGameChannel(msgEvent, msgEvent.getMember().getEffectiveName() + "s-countdown-conundrum");
-        String winner = "";
-        String[][] conundrums = getConundrums();
-        Random r = new Random();
-        int index = r.nextInt(conundrums.length);
-        embed = new EmbedBuilder();
-        embed.setColor(Core.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-        embed.setTitle("Countdown Conundrum");
-        embed.setThumbnail("https://i.imgur.com/0uNRZWG.png");
-        embed.setDescription("Find the anagram before time runs out!\n\n***"+conundrums[index][0].toUpperCase()+"***");
-        embedMsg = channel.sendMessage(embed.build()).complete();
-
-        Runnable timer = () ->
+        try
         {
-            seconds = 0;
+            String winner = "";
+            String[] conundrum = getConundrum();
+            Random r = new Random();
+            embed = new EmbedBuilder();
+            embed.setColor(Core.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setTitle("Countdown Conundrum");
+            embed.setThumbnail("https://i.imgur.com/0uNRZWG.png");
+            embed.setDescription("Find the anagram before time runs out!\n\n***"+conundrum[1].toUpperCase()+"***");
+            embedMsg = channel.sendMessage(embed.build()).complete();
+
+            Runnable timer = () ->
+            {
+                seconds = 0;
+                while (seconds <= 30)
+                {
+                    try
+                    {
+                        switch (seconds)
+                        {
+                            case 30:
+                                embed.setThumbnail("https://i.imgur.com/eMrORg8.png");
+                                embedMsg.editMessage(embed.build()).queue();
+                                break;
+                            case 23:
+                                embed.setThumbnail("https://i.imgur.com/F2deeEY.png");
+                                embedMsg.editMessage(embed.build()).queue();
+                                break;
+                            case 15:
+                                embed.setThumbnail("https://i.imgur.com/JpwwNrY.png");
+                                embedMsg.editMessage(embed.build()).queue();
+                                break;
+                            case 8:
+                                embed.setThumbnail("https://i.imgur.com/llmbGHa.png");
+                                embedMsg.editMessage(embed.build()).queue();
+                                break;
+
+                        }
+                        Thread.sleep(1000);
+                        seconds++;
+                    }
+                    catch (InterruptedException e)
+                    {
+                        //Game end.
+                        return;
+                    }
+
+                }
+            };
+            Thread timerThread = new Thread(timer);
+            timerThread.start();
+            MessageManager msgManager = new MessageManager();
             while (seconds <= 30)
             {
-                try
+                Message message = msgManager.getNextMessage(channel, 1000);
+                if (message != null)
                 {
-                    switch (seconds)
+                    String answer = message.getContentDisplay();
+
+                    if (answer.length() == 5 && answer.toLowerCase().endsWith("quit"))
                     {
-                        case 30:
-                            embed.setThumbnail("https://i.imgur.com/eMrORg8.png");
-                            embedMsg.editMessage(embed.build()).queue();
-                            break;
-                        case 23:
-                            embed.setThumbnail("https://i.imgur.com/F2deeEY.png");
-                            embedMsg.editMessage(embed.build()).queue();
-                            break;
-                        case 15:
-                            embed.setThumbnail("https://i.imgur.com/JpwwNrY.png");
-                            embedMsg.editMessage(embed.build()).queue();
-                            break;
-                        case 8:
-                            embed.setThumbnail("https://i.imgur.com/llmbGHa.png");
-                            embedMsg.editMessage(embed.build()).queue();
-                            break;
-
+                        timerThread.interrupt();
                     }
-                    Thread.sleep(1000);
-                    seconds++;
-                }
-                catch (InterruptedException e)
-                {
-                    //Game end.
-                    return;
-                }
 
+                    if (answer.equalsIgnoreCase(conundrum[0]))
+                    {
+                        winner = message.getMember().getEffectiveName();
+                        timerThread.interrupt();
+                        break;
+                    }
+                    else if (answer.length() == 9)
+                    {
+                        channel.sendMessage("Good guess, but that's not it.").queue();
+                    }
+                }
             }
-        };
-        Thread timerThread = new Thread(timer);
-        timerThread.start();
-        MessageManager msgManager = new MessageManager();
-        while (seconds <= 30)
-        {
-            Message message = msgManager.getNextMessage(channel, 1000);
-            if (message != null)
+
+            if (!winner.equals(""))
             {
-                String answer = message.getContentDisplay();
-
-                if (answer.length() == 5 && answer.toLowerCase().endsWith("quit"))
-                {
-                    timerThread.interrupt();
-                }
-
-                if (answer.equalsIgnoreCase(conundrums[index][1]))
-                {
-                    winner = message.getMember().getEffectiveName();
-                    timerThread.interrupt();
-                    break;
-                }
-                else if (answer.length() == 9)
-                {
-                    channel.sendMessage("Good guess, but that's not it.").queue();
-                }
+                embed.setThumbnail("https://i.imgur.com/scKHMRb.png");
+                embed.setDescription("Congratulations! The word was "+conundrum[0]+".\n"+winner+" got it in just "+seconds+" seconds!");
+                channel.sendMessage(embed.build()).queue();
+            }
+            else
+            {
+                embed.setThumbnail("https://i.imgur.com/KwjqNkH.png");
+                embed.setDescription("Oh no! You were beaten by the clock. The word was "+conundrum[0]+".\nBetter luck next time!");
+                channel.sendMessage(embed.build()).queue();
             }
         }
-
-        if (!winner.equals(""))
+        catch (IOException e)
         {
-            embed.setThumbnail("https://i.imgur.com/scKHMRb.png");
-            embed.setDescription("Congratulations! The word was "+conundrums[index][1]+".\n"+winner+" got it in just "+seconds+" seconds!");
-            channel.sendMessage(embed.build()).queue();
+            channel.sendMessage("An unexpected error occured.").queue();
+            e.printStackTrace();
         }
-        else
-        {
-            embed.setThumbnail("https://i.imgur.com/KwjqNkH.png");
-            embed.setDescription("Oh no! Looks like you were beaten by the clock.\nBetter luck next time!");
-            channel.sendMessage(embed.build()).queue();
-        }
-
         super.deleteGameChannel(msgEvent, channel);
     }
 
     /**
-     * Gets the list of conundrums, where the first dimension defines the conundrum, with the second dimension having the anagram in index 0, and the answer in index 1.
+     * Gets the list of conundrum, where the first array index is the answer, and the second is the anagram.
      * @return String[][] - The conundrums
      */
-    private String[][] getConundrums() //TODO: Generate conundrums from word list?
+    private String[] getConundrum() throws IOException
     {
-        String[][] conundrums = new String[40][2];
-        conundrums[0][0] = "Soartrip";
-        conundrums[0][1] = "Airports";
-        conundrums[1][0] = "Toooften";
-        conundrums[1][1] = "Footnote";
-        conundrums[2][0] = "Froglegs";
-        conundrums[2][1] = "Floggers";
-        conundrums[3][0] = "Girltina";
-        conundrums[3][1] = "Tailing";
-        conundrums[4][0] = "Peterdad";
-        conundrums[4][1] = "Departed";
-        conundrums[5][0] = "Scotchyip";
-        conundrums[5][1] = "Psychotic";
-        conundrums[6][0] = "Rifledick";
-        conundrums[6][1] = "Flickered";
-        conundrums[7][0] = "Atellyhit";
-        conundrums[7][1] = "Lethality";
-        conundrums[8][0] = "Wontcnoud";
-        conundrums[8][1] = "Countdown";
-        conundrums[9][0] = "Drumuncon";
-        conundrums[9][1] = "Conundrum";
-        conundrums[10][0] = "Crewonset";
-        conundrums[10][1] = "Sweetcorn";
-        conundrums[11][0] = "Minigramt";
-        conundrums[11][1] = "Immigrant";
-        conundrums[12][0] = "Hashpipes";
-        conundrums[12][1] = "Shipshape";
-        conundrums[13][0] = "Itmbignre";
-        conundrums[13][1] = "Timbering";
-        conundrums[14][0] = "Amusingmy";
-        conundrums[14][1] = "Gymnasium";
-        conundrums[15][0] = "Largebaps";
-        conundrums[15][1] = "Graspable";
-        conundrums[16][0] = "Sentminor";
-        conundrums[16][1] = "Innermost";
-        conundrums[17][0] = "Drapospel";
-        conundrums[17][1] = "Prolapsed";
-        conundrums[18][0] = "Dickhewer";
-        conundrums[18][1] = "Prolapsed";
-        conundrums[19][0] = "Flungfish";
-        conundrums[19][1] = "Shuffling";
-        conundrums[20][0] = "Cuzzsball";
-        conundrums[20][1] = "Scuzzball";
-        conundrums[21][0] = "Juboxkees";
-        conundrums[21][1] = "Jukeboxes";
-        conundrums[22][0] = "Pizzaarpa";
-        conundrums[22][1] = "Paparazzi";
-        conundrums[23][0] = "Repsybarr";
-        conundrums[23][1] = "Raspberry";
-        conundrums[24][0] = "Coppypock";
-        conundrums[24][1] = "Poppycock";
-        conundrums[25][0] = "Jifyectob";
-        conundrums[25][1] = "Objectify";
-        conundrums[26][0] = "Offockkns";
-        conundrums[26][1] = "Knockoffs";
-        conundrums[27][0] = "Fillyjesh";
-        conundrums[27][1] = "Jellyfish";
-        conundrums[28][0] = "Muglebubb";
-        conundrums[28][1] = "Bubblegum";
-        conundrums[29][0] = "Mooschzer";
-        conundrums[29][1] = "Schmoozer";
-        conundrums[30][0] = "Dizeminim";
-        conundrums[30][1] = "Minimized";
-        conundrums[31][0] = "Druowbszz";
-        conundrums[31][1] = "Buzzwords";
-        conundrums[32][0] = "Bornmaity";
-        conundrums[32][1] = "Abnormity";
-        conundrums[33][0] = "Badtedack";
-        conundrums[33][1] = "Backdated";
-        conundrums[34][0] = "Bitialies";
-        conundrums[34][1] = "Abilities";
-        conundrums[35][0] = "Repledmas";
-        conundrums[35][1] = "Resampled";
-        conundrums[36][0] = "Rarhingbor";
-        conundrums[36][1] = "Harboring";
-        conundrums[37][0] = "Finlentus";
-        conundrums[37][1] = "Influents";
-        conundrums[38][0] = "Furyllest";
-        conundrums[38][1] = "Restfully";
-        conundrums[39][0] = "Vocelkits";
-        conundrums[39][1] = "Livestock";
-        return conundrums;
+        try
+        {
+            String[] conundrum = new String[2];
+            File conundrumFile = new File(getClass().getResource("/game/conundrums.txt").toURI());
+            double lines = Files.lines(conundrumFile.toPath()).count();
+            Scanner scanner = new Scanner(conundrumFile);
+            long conundrumIndex = Math.round(Math.random()*lines);
+            while (conundrumIndex >= 0)
+            {
+                conundrum[0] = scanner.next();
+                conundrumIndex--;
+            }
+            scanner.close();
+
+            byte[] conundrumChars = conundrum[0].getBytes();
+            Random r = new Random();
+            for (int i = conundrumChars.length - 1; i > 0; i--)
+            {
+                int charIndex = r.nextInt(i + 1);       //This will eventually go to between 0 or 1, however those values (most likely) will have already been swapped.
+                byte temp = conundrumChars[charIndex];
+                conundrumChars[charIndex] = conundrumChars[i];
+                conundrumChars[i] = temp;
+            }
+            conundrum[1] = new String(conundrumChars, StandardCharsets.UTF_8);
+
+            return conundrum;
+        }
+        catch (URISyntaxException e)
+        {
+            logger.error("The Conundrums file is missing or corrupt. Countdown Conundrums cannot function.");
+            throw new IOException();
+        }
+
     }
 
 }
