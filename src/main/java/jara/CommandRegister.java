567b233bd3761a8af0a012e1a4f80b629f81c4e7
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-10-21 19:27:16 +0100

    Add PassTheBomb game
    This is only a rough implementation, as this game *requires* active players to be set unlike the others, but the current implementation of this selection isn't very user friendly, or obvious that it is required. So, a more elegant player selection solution should be divised.

--- src/main/java/jara/CommandRegister.java
+++ src/main/java/jara/CommandRegister.java
@@ -12,10 +12,7 @@ import commands.Help;
 import commands.admin.AddCommand;
 import commands.admin.config.ConfigMain;
 import commands.audio.*;
-import commands.games.Countdown;
-import commands.games.CountdownConundrum;
-import commands.games.Hangman;
-import commands.games.LastWord;
+import commands.games.*;
 import commands.toys.*;
 import commands.utility.*;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
@@ -89,6 +86,7 @@ public class CommandRegister
 			register.add(new CommandAttributes("AddCommand", "Adds a custom command.", AddCommand.class, new String[] {"CustomCommand"}, ADMIN, true));
 			register.add(new CommandAttributes("CustomCommand", "Custom Command Template.", CustomCommand.class, new String[0], NOGROUP, false)); //TODO: Make this disableable
 			register.add(new CommandAttributes("LastWord", "Get the last word in.", LastWord.class, new String[] {"TheLastWord", "Scattergories", "Topics"}, GAMES, true));
+			register.add(new CommandAttributes("PassTheBomb", "Quick! Pass the bomb!", PassTheBomb.class, new String[] {"HotPotato", "BombPass"}, GAMES, true));
 
 			/*
 					Sort the commands into alphabetical order based on their keys
