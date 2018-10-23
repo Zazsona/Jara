d3a604a7d6030977d8cedb4d05b06a557e589492
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-10-23 19:14:24 +0100

    Complete CustomCommand class.

--- src/main/java/commands/CustomCommand.java
+++ src/main/java/commands/CustomCommand.java
@@ -1,10 +1,15 @@
 package commands;
 
+import commands.audio.Play;
 import configuration.GuildSettingsJson;
 import configuration.SettingsUtil;
 import jara.Core;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
+
+import java.util.ArrayList;
 
 public class CustomCommand extends Command
 {
@@ -12,18 +17,54 @@ public class CustomCommand extends Command
     @Override
     public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
     {
-        String key = getKey(msgEvent.getGuild().getId(), parameters[0]);
-        GuildSettingsJson.CustomCommandConfig customCommand = SettingsUtil.getGuildSettings(msgEvent.getGuild().getId()).getCustomCommand(key);
+        try
+        {
+            String key = getKey(msgEvent.getGuild().getId(), parameters[0]);
+            GuildSettingsJson.CustomCommandConfig customCommand = SettingsUtil.getGuildSettings(msgEvent.getGuild().getId()).getCustomCommand(key);
 
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.setColor(Core.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-        embed.setTitle(key);
-        embed.setDescription(customCommand.getMessage());
-        msgEvent.getChannel().sendMessage(embed.build()).queue();
+            if (!customCommand.getMessage().equals(""))
+            {
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setColor(Core.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+                embed.setTitle(key);
+                embed.setDescription(customCommand.getMessage());
+                msgEvent.getChannel().sendMessage(embed.build()).queue();
+            }
+            if (customCommand.getRoles().size() > 0)
+            {
+                ArrayList<Role> roles = new ArrayList<>();
+                for (String roleID : customCommand.getRoles())
+                {
+                    roles.add(msgEvent.getGuild().getRoleById(roleID));
+                }
+                msgEvent.getGuild().getController().addRolesToMember(msgEvent.getMember(), roles).queue();
+            }
+            if (!customCommand.getAudioLink().equals(""))
+            {
+                new Play().run(msgEvent, SettingsUtil.getGuildCommandPrefix(msgEvent.getGuild().getId())+"play", customCommand.getAudioLink());
+            }
+        }
+        catch (NullPointerException e)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(Core.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setTitle("Error");
+            embed.setDescription("You must specify a custom command key."); //Reeeeally I'd rather they not use this method anyway, but hey, no harm in having the option.
+            msgEvent.getChannel().sendMessage(embed.build()).queue();
+        }
+        catch (InsufficientPermissionException e)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(Core.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setTitle("Error");
+            embed.setDescription("I do not have sufficient permissions / role hierarchy to fully perform the command.");
+            msgEvent.getChannel().sendMessage(embed.build()).queue();
+        }
     }
+
+
     private String getKey(String guildID, String call)
     {
         return call.replaceFirst(SettingsUtil.getGuildCommandPrefix(guildID).toString(), "");
     }
-    //TODO: Implement this properly (Tidy up, allow for multiple custom command types (Role assignment, message response, voice channel track)
 }
