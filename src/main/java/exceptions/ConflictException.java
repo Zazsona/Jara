d86ea5dbbf8eea03612ea18f6cb405c1b802c721
Zazsona <40435178+Zazsona@users.noreply.github.com>
2018-11-28 21:58:14 +0000

    Add ConflictException

---
+++ src/main/java/exceptions/ConflictException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+/**
+ * An exception indicating two values within different objects match when they cannot.
+ */
+public class ConflictException extends Exception
+{
+    public ConflictException(String message)
+    {
+        super(message);
+    }
+}
