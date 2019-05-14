6f8c5c100e8a368ececcc1c698f746b4ae9e4b65
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-05-14 18:10:25 +0100

    Add the ability for anyone to join game channels.
    This is a breaking change for modules, though. Whoopee!

--- src/main/java/commands/GameCommand.java
+++ src/main/java/commands/GameCommand.java
@@ -2,12 +2,14 @@ package commands;
 
 import configuration.GuildSettings;
 import configuration.SettingsUtil;
-import net.dv8tion.jda.core.entities.Category;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.core.exceptions.GuildUnavailableException;
 import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +22,10 @@ import java.util.TimerTask;
 
 public abstract class GameCommand extends Command //A base class to build commands from.
 {
+	private static HashMap<String, TextChannel> gameChannelMap;
+	private static GameReactionListener gameReactionListener;
+	private Message gameMsg;
+
 	/**
 	 * A timer of configurable length. It counts the time since the last user interaction in the game channel. If it expires, the channel is deleted.
 	 */
@@ -42,11 +48,13 @@ public abstract class GameCommand extends Command //A base class to build comman
 	 * @param channelName the channel to create
 	 * @return the channel to run the game in
 	 */
-	protected TextChannel createGameChannel(GuildMessageReceivedEvent msgEvent, String channelName)
+	protected TextChannel createGameChannel(GuildMessageReceivedEvent msgEvent, String channelName, Member... players)
 	{
 		Logger logger = LoggerFactory.getLogger(GameCommand.class);
 		GuildSettings guildSettings = SettingsUtil.getGuildSettings(msgEvent.getGuild().getId());
 		String gameCategoryID = guildSettings.getGameCategoryId();
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
 		if (!guildSettings.isGameChannelsEnabled())
 		{
 			if (guildSettings.isConcurrentGameInChannelAllowed())
@@ -54,7 +62,8 @@ public abstract class GameCommand extends Command //A base class to build comman
 				channelsRunningGames.putIfAbsent(msgEvent.getGuild().getId(), new ArrayList<>());
 				if (channelsRunningGames.get(msgEvent.getGuild().getId()).contains(msgEvent.getChannel().getId()))
 				{
-					msgEvent.getChannel().sendMessage("There's already a game running in this channel.").queue();
+					embed.setDescription("There's already a game running in this channel.");
+					msgEvent.getChannel().sendMessage(embed.build()).queue();
 					Thread.currentThread().interrupt();
 					return null;
 				}
@@ -67,13 +76,18 @@ public abstract class GameCommand extends Command //A base class to build comman
 		}
 		else
 		{
+			if (gameChannelMap == null)
+			{
+				gameChannelMap = new HashMap<>();
+				gameReactionListener = new GameReactionListener();
+				msgEvent.getJDA().addEventListener(gameReactionListener);
+			}
 			Category gameCategory = msgEvent.getGuild().getCategoryById(gameCategoryID);
 			if (gameCategory != null)
 			{
 				try
 				{
 					TextChannel channel = (TextChannel) gameCategory.createTextChannel(channelName).complete();
-					msgEvent.getChannel().sendMessage("Game started in "+channel.getAsMention()).queue();
 					int channelTimeout = Integer.parseInt(SettingsUtil.getGuildSettings(msgEvent.getGuild().getId()).getGameChannelTimeout());
 					if (channelTimeout != 0)
 					{
@@ -95,11 +109,21 @@ public abstract class GameCommand extends Command //A base class to build comman
 							}
 						}, channelTimeout/2, channelTimeout/2);
 					}
+					channel.putPermissionOverride(channel.getGuild().getPublicRole()).setDeny(Permission.MESSAGE_READ).queue();
+					for (Member player : players)
+					{
+						channel.createPermissionOverride(player).setAllow(Permission.MESSAGE_READ).queue();
+					}
+					embed.setDescription(msgEvent.getMember().getEffectiveName()+" has started "+channel.getAsMention()+".\nReact with :game_die: to join.");
+					gameMsg = msgEvent.getChannel().sendMessage(embed.build()).complete();
+					gameMsg.addReaction("\uD83C\uDFB2").queue();
+					gameChannelMap.put(gameMsg.getId(), channel);
 					return channel;
 				}
 				catch (InsufficientPermissionException e)
 				{
-					msgEvent.getChannel().sendMessage("A game category has been set up and exists, but I don't have permission to create channels there!").queue();
+					embed.setDescription("A game category has been set up and exists, but I don't have permission to create channels and add players there!");
+					msgEvent.getChannel().sendMessage(embed.build()).queue();
 					Thread.currentThread().interrupt();
 					return null;
 				}
@@ -122,7 +146,8 @@ public abstract class GameCommand extends Command //A base class to build comman
 			{
 				try
 				{
-					msgEvent.getChannel().sendMessage("The game category has disappeared. Disabling game channels. You can re-enable these in the config.").queue();
+					embed.setDescription("The game category has disappeared. Disabling game channels. You can re-enable these in the config.");
+					msgEvent.getChannel().sendMessage(embed.build()).queue();
 					logger.info("Guild "+msgEvent.getGuild().getId()+"'s game category for the saved id doesn't exist. Removing from config...");
 					guildSettings.setGameCategoryId("");
 					guildSettings.setUseGameChannels(false);
@@ -160,6 +185,8 @@ public abstract class GameCommand extends Command //A base class to build comman
 				e.printStackTrace();
 			}
 			channel.delete().queue();
+			gameChannelMap.remove(gameMsg.getId());
+			gameMsg.delete().queue();
 		}
 		else
 		{
@@ -169,5 +196,31 @@ public abstract class GameCommand extends Command //A base class to build comman
 			}
 		}
 		return;
+	}
+
+	private class GameReactionListener extends ListenerAdapter
+	{
+		@Override
+		public void onMessageReactionAdd(MessageReactionAddEvent mra)
+		{
+			try
+			{
+				if (mra.getChannel().getMessageById(mra.getMessageIdLong()).complete().getMember().equals(mra.getGuild().getSelfMember())) //Since this method checks all reactions, this at least limits it to bot only ones
+				{
+					if (gameChannelMap.containsKey(mra.getMessageId()))
+					{
+						PermissionOverride po = gameChannelMap.get(mra.getMessageId()).getPermissionOverride(mra.getMember());
+						if (po != null && !po.getAllowed().contains(Permission.MESSAGE_READ))
+						{
+							gameChannelMap.get(mra.getMessageId()).createPermissionOverride(mra.getMember()).setAllow(Permission.MESSAGE_READ).queue();
+						}
+					}
+				}
+			}
+			catch (InsufficientPermissionException e)
+			{
+				mra.getChannel().sendMessage("Error: Unable to add player due to insufficient permissions.").queue();
+			}
+		}
 	}
 }
